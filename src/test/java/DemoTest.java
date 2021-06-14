6f28bc77572ce2a0aadc1ee0c68ae8c1b6b8b405
Sean Jones <jones@msoe.edu>
2021-06-14 14:54:40 -0500

    Added messages to tests

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,16 +1,19 @@
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import jones.Demo;
 
 public class DemoTest {
     @Test
+    @DisplayName("Multiply By Five")
     void testMultiplyByFive() {
-        Assertions.assertEquals(25, Demo.multiplyByFive(5));
+        Assertions.assertEquals(25, Demo.multiplyByFive(5), "Multiply By Five Failed");
     }
 
     @Test
+    @DisplayName("Add Two")
     void testAddTwo() {
-        Assertions.assertEquals(7, Demo.addTwo(5));
+        Assertions.assertEquals(7, Demo.addTwo(5), "Add Two Failed");
     }
 }
